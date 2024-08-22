8ec51ea5c32d02708e7dafeb55eb06dc54ef4630
ana rivera <anaruth.riveras@unitec.edu>
2024-08-21 23:26:50 -0600

    Arreglado el problema del timer de despedida que se aplicaba a las demas pantallas

--- Proyecto_ATM/PantallaDespedida.cs
+++ Proyecto_ATM/PantallaDespedida.cs
@@ -12,6 +12,7 @@ namespace Proyecto_ATM
 {
     public partial class PantallaDespedida : UserControl
     {
+        private System.Windows.Forms.Timer Relojito;
         public Action OnTimeout { get; set; }
         public PantallaDespedida()
         {
@@ -25,19 +26,30 @@ namespace Proyecto_ATM
             label2.Location = new Point(90, 205);
             label1.Location = new Point(96, 255);
 
-
-            //Timer de espera para la pantalla
-            System.Windows.Forms.Timer Relojito = new System.Windows.Forms.Timer();
+            Relojito = new System.Windows.Forms.Timer();
             Relojito.Interval = (10 * 1000);
             Relojito.Tick += new EventHandler(TimesUp);
-            Relojito.Start();
 
+        }
+        protected override void OnVisibleChanged(EventArgs e)
+        {
+            if (this.Visible)
+            {
+                Relojito.Start(); 
+            }
+            else
+            {
+                Relojito.Stop(); 
+            }
         }
 
         private void TimesUp(object sender, EventArgs e)
         {
+            Relojito.Stop();
+            Relojito.Dispose();
             this.Hide();
             OnTimeout?.Invoke();
+            
         }
 
     }
