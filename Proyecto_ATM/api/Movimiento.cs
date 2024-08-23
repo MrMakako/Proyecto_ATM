3813478b98bcd2c0d8e566d79f43aefa11d8ac46
diego <diego@DIEGO-LAPTOP>
2024-08-23 16:47:23 -0600

    creacion de la clase movimienot

---
+++ Proyecto_ATM/api/Movimiento.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_ATM.api
+{
+    internal class Movimiento
+    {
+        private string numero_cuenta;
+        private Conector conector;
+        private string pin;
+
+        public Movimiento(string numero_cuenta,string pin,Conector conector) {
+            this.numero_cuenta = numero_cuenta;
+            this.conector = conector;
+            this.pin = pin;
+
+        }
+        public bool retiro(int monto) {
+
+            try
+            {
+
+                // Realizar query que deduzca dinero de una cuenta.
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return false;
+       
+        }
+
+        public double consultar_saldo(string numero_cuenta) {
+            
+            //consultar el saldo de la cuneta.
+            return 0.0;
+        
+        }
+        public string getNumero_cuenta() { return numero_cuenta; }
+        public Conector getConector() { return conector; }
+
+
+
+
+
+
+
+    }
+}
