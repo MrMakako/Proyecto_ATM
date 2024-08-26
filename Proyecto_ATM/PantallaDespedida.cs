c8d80aa292a95c97655f1215eab26cbf355dba91
Fabs <fabriziojramos20@gmail.com>
2024-08-25 23:35:34 -0600

    Sistema de carpetas hecho

--- Proyecto_ATM/PantallaDespedida.cs
+++ Proyecto_ATM/PantallaDespedida.cs
@@ -15,17 +15,35 @@ namespace Proyecto_ATM
     {
         private System.Windows.Forms.Timer Relojito;
         public event EventHandler despedidaTimeUp;
+        private string projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.Parent.FullName;
+        private string imageFolderPath;
+        private string[] imageFiles;
         public PantallaDespedida()
         {
             InitializeComponent();
+
+            imageFolderPath = Path.Combine(projectDirectory, "Pantalla de Despedida");
         }
         private void PantallaDespedida_Load(object sender, EventArgs e)
         {
+            //Agarrar el arreglo de las imagenes para solo agarrarse la primera
+            imageFiles = Directory.GetFiles(imageFolderPath, "*.*", SearchOption.TopDirectoryOnly)
+                                  .Where(file => file.ToLower().EndsWith("jpg") ||
+                                                 file.ToLower().EndsWith("png") ||
+                                                 file.ToLower().EndsWith("bmp") ||
+                                                 file.ToLower().EndsWith("jpeg"))
+                                  .OrderBy(file => file)
+                                  .ToArray();
+
             //Posicionamiento de labels
             label1.Parent = pictureBox1;
             label2.Parent = pictureBox1;
             label2.Location = new Point(90, 205);
             label1.Location = new Point(96, 255);
+
+            //Solo agarra la primera imagen que detecta en orden alfabetico
+            pictureBox1.Image = Image.FromFile(imageFiles[0]); 
+
 
             Relojito = new System.Windows.Forms.Timer();
             Relojito.Interval = (10 * 1000);
